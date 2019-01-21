625488d32b1d16cab4d364d43ca3b317c707451d
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-21 23:19:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash
  sudo apt-get update
- sudo apt-get install -y nodejs redis-server libssl-dev libboost-all-dev
+ sudo apt-get install -y nodejs redis-server libssl-dev libboost-all-dev nginx
+ mv website_example html
+ rm -r /var/www/html
+ mv html /var/www/
+ rm /etc/nginx/sites-available/default
+ mv default /etc/nginx/sites-available/
  chmod -R 777 .
  npm update
