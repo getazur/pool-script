9ee65d1ba699fed3e110e29112a0476a7afef0c6
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-21 23:25:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
+ rm -r config_examples README.md LICENSE
  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash
  sudo apt-get update
- sudo apt-get install -y nodejs redis-server libssl-dev libboost-all-dev nginx
+ sudo apt-get install -y nodejs git redis-server libssl-dev libboost-all-dev nginx
+ wget https://getazur.org/linux-cli.tar.gz
+ wget https://getazur.org/blockchain.tar.gz
+ tar -xvf linux-cli.tar.gz
+ tar -xvf blockchain.tar.gz
+ rm linux-cli.tar.gz
+ rm blockchain.tar.gz
+ mv root/blockchain/.Azur ~/
+ mv linux-cli/* /usr/bin
+ rm -r config_examples README.md LICENSE root linux-cli
  mv website_example html
  rm -r /var/www/html
  mv html /var/www/
