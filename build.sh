379787aa4301867e11c32ddecd4bfb1b230ca5e3
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-21 23:41:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,6 @@
  sudo add-apt-repository ppa:certbot/certbot -y
  sudo apt-get update
  sudo apt-get install -y nodejs git redis-server libssl-dev libboost-all-dev nginx python-certbot-nginx
- wget https://getazur.org/linux-cli.tar.gz
- wget https://getazur.org/blockchain.tar.gz
- tar -xvf linux-cli.tar.gz
- tar -xvf blockchain.tar.gz
- rm linux-cli.tar.gz
- rm blockchain.tar.gz
- mv root/blockchain/.Azur ~/
- mv linux-cli/* /usr/bin
- rm -r config_examples README.md LICENSE root linux-cli
  mv website_example html
  rm -r /var/www/html
  mv html /var/www/
