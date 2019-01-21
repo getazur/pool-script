9cdf8956cbd575799ceaab62f1a93887964ac579
Azur Network <45802475+getazur@users.noreply.github.com>
2019-01-21 23:16:57 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash
+ sudo apt-get update
+ sudo apt-get install -y nodejs redis-server libssl-dev libboost-all-dev
+ chmod -R 777 .
+ npm update
